a7824bcb6e85fe14027b5fdc8d25cfd30723255a
lws803 <lws803@gmail.com>
2023-02-15 20:38:49 -0500

    added firebase and config

---
+++ src/shared/config/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDBLdrgNrgfswCH051ReCuBTg3CXtq_IVQ",
+  authDomain: "ase-project-team-42.firebaseapp.com",
+  projectId: "ase-project-team-42",
+  storageBucket: "ase-project-team-42.appspot.com",
+  messagingSenderId: "845006074385",
+  appId: "1:845006074385:web:9a8115c9ab7704a0e777b0",
+};
