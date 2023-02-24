ee17bde95b4f51497ab4dd0c573b90d0cb5baa82
lws803 <lws803@gmail.com>
2023-02-24 12:45:15 -0500

    fixed private key encoding

--- src/api/shared/firebaseApp.ts
+++ src/api/shared/firebaseApp.ts
@@ -3,10 +3,12 @@ import { initializeApp, cert, getApp, getApps } from "firebase-admin/app";
 export const getFirebaseApp = () => {
   if (getApps().length > 0) return getApp();
 
+  const { private_key } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY!);
+
   return initializeApp({
     credential: cert({
       projectId: "ase-project-team-42",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: private_key,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     projectId: "ase-project-team-42",
