829df8d84c04a750de0d725bd78b6725f7ef6e6f
lws803 <lws803@gmail.com>
2023-03-09 20:00:54 -0500

    only load language switch client side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import React, { Children, useEffect, useState } from "react";
-import { MenuProps, Select, Switch, Tooltip } from "antd";
+import React, { useEffect, useState } from "react";
+import { MenuProps, Switch, Tooltip } from "antd";
 import { Layout, Menu, theme, ConfigProvider } from "antd";
-import { BarsOutlined } from '@ant-design/icons'; 
+import { BarsOutlined } from "@ant-design/icons";
 import { AuthContext, useAuth } from "@/modules/auth";
 import { PAGES } from "../constants";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import {
   DietIcon,
   HomeIcon,
@@ -17,10 +17,10 @@ import {
 } from "../pageComponent/Icons";
 import { getKeyByValue } from "@/components/utils";
 
-// import i18n (needs to be bundled ;)) 
-import '../../i18n';
+// import i18n (needs to be bundled ;))
+import "../../i18n";
 
-const { Content, Footer, Sider } = Layout;
+const { Content, Sider } = Layout;
 
 const items: MenuProps["items"] = [
   HomeIcon,
@@ -38,11 +38,11 @@ export default function App({ Component, pageProps }: AppProps) {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
-  
-  const { signIn, authState, signOut  } = useAuth();
-  
-  const { t, i18n } = useTranslation('', { useSuspense: false });
-  const [currentLng, setCurrentLang] = useState('en-CA');
+
+  const { signIn, authState, signOut } = useAuth();
+
+  const { t, i18n } = useTranslation("", { useSuspense: false });
+  const [currentLng, setCurrentLang] = useState("en-CA");
 
   const sideBarRef = React.useRef<any>(null);
 
@@ -54,16 +54,16 @@ export default function App({ Component, pageProps }: AppProps) {
 
   const handleSideBarControl = () => {
     sideBarRef.current?.classList.toggle("sider-container-active");
-  }
+  };
 
   function handleChangeLanguage(checked: boolean) {
-    i18n.changeLanguage(checked? 'en-CA' : 'fr-FR');
-  };
+    i18n.changeLanguage(checked ? "en-CA" : "fr-FR");
+  }
 
   //TO DO: fix the logic to make the btn show the right language when the app 1st loads
   useEffect(() => {
     if (currentLng !== i18n.language) setCurrentLang(i18n.language);
-  },[])
+  }, [currentLng, i18n.language]);
 
   return (
     <AuthContext>
@@ -102,12 +102,15 @@ export default function App({ Component, pageProps }: AppProps) {
               >
                 <Menu
                   mode="inline"
-                  selectedKeys={[getKeyByValue(PAGES, router.pathname) || Object.keys(PAGES)[0]]}
+                  selectedKeys={[
+                    getKeyByValue(PAGES, router.pathname) ||
+                      Object.keys(PAGES)[0],
+                  ]}
                   items={items}
                   onClick={handleClick}
                 />
-                <div className='functional-btns'>
-                  <div className='functional-btns-lang'>
+                <div className="functional-btns">
+                  <div className="functional-btns-lang">
                     <ConfigProvider
                       theme={{
                         token: {
@@ -117,26 +120,39 @@ export default function App({ Component, pageProps }: AppProps) {
                         components: {},
                       }}
                     >
-                      <Switch 
-                        checkedChildren="EN"
-                        unCheckedChildren="FR"
-                        defaultChecked
-                        onChange={handleChangeLanguage}
-                      />
+                      {i18n.isInitialized && typeof window !== "undefined" && (
+                        <Switch
+                          checkedChildren="EN"
+                          unCheckedChildren="FR"
+                          checked={i18n.language === "en-CA"}
+                          onChange={handleChangeLanguage}
+                        />
+                      )}
                     </ConfigProvider>
                   </div>
-                  <Tooltip placement="right" title={'Log out'} className='logout-btn'>
-                    <button onClick={authState === "signedOut" ? signIn : signOut}>
-                      <img src="https://img.icons8.com/ios/50/FFFFFF/logout-rounded-left.png" alt='logout-btn'/>
+                  <Tooltip
+                    placement="right"
+                    title={"Log out"}
+                    className="logout-btn"
+                  >
+                    <button
+                      onClick={authState === "signedOut" ? signIn : signOut}
+                    >
+                      <img
+                        src="https://img.icons8.com/ios/50/FFFFFF/logout-rounded-left.png"
+                        alt="logout-btn"
+                      />
                     </button>
                   </Tooltip>
                 </div>
               </Sider>
-              <button className="sider-container-button" onClick={handleSideBarControl}>
-                <BarsOutlined className="sider-container-button-icon"/>
+              <button
+                className="sider-container-button"
+                onClick={handleSideBarControl}
+              >
+                <BarsOutlined className="sider-container-button-icon" />
               </button>
             </div>
-            
           </ConfigProvider>
           <Layout
             className="site-layout"
