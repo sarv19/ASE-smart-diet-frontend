f6cdbd8bba994f206c39a348f47b9373627bfe9c
lws803 <lws803@gmail.com>
2023-03-14 22:54:09 -0400

    load BE data as initial values

--- src/components/DietSettings.tsx
+++ src/components/DietSettings.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { Button, Form, InputNumber, Space } from "antd";
+import { Button, Form, InputNumber, Space, Spin } from "antd";
 import { useTranslation } from "react-i18next";
 
 import * as settings from "@/modules/settings/actions";
@@ -20,14 +20,12 @@ const DietSettings = () => {
     },
   });
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryFn: async () =>
       settings.getDiet(currentUser ? await currentUser.getIdToken() : ""),
     queryKey: [settings.getDiet.key, currentUser?.uid],
     enabled: !!currentUser,
   });
-
-  console.log(data);
 
   const onFinish = (values: any) => {
     mutate({
@@ -42,74 +40,81 @@ const DietSettings = () => {
 
   return (
     <div>
-      <Form
-        name="basic"
-        labelCol={{ span: 5 }}
-        style={{ width: "100%" }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item label={<div>{t("Calories")}</div>}>
-          <Space>
-            <Form.Item noStyle name="minCal">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-            <Form.Item noStyle name="maxCal">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-          </Space>
-        </Form.Item>
-        <Form.Item label={<div>{t("Protein (mg)")}</div>}>
-          <Space>
-            <Form.Item noStyle name="minProtein">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-            <Form.Item noStyle name="maxProtein">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-          </Space>
-        </Form.Item>
-        <Form.Item label={<div>{t("Carbohydrates (mg)")}</div>}>
-          <Space>
-            <Form.Item noStyle name="minCarb">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-            <Form.Item noStyle name="maxCarb">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-          </Space>
-        </Form.Item>
+      {isLoading ? (
+        <Spin />
+      ) : (
+        <Form
+          name="basic"
+          labelCol={{ span: 5 }}
+          style={{ width: "100%" }}
+          initialValues={{
+            minCal: data.targetCaloriesMin,
+            maxCal: data.targetCaloriesMax,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item label={<div>{t("Calories")}</div>}>
+            <Space>
+              <Form.Item noStyle name="minCal">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+              <Form.Item noStyle name="maxCal">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+            </Space>
+          </Form.Item>
+          <Form.Item label={<div>{t("Protein (mg)")}</div>}>
+            <Space>
+              <Form.Item noStyle name="minProtein">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+              <Form.Item noStyle name="maxProtein">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+            </Space>
+          </Form.Item>
+          <Form.Item label={<div>{t("Carbohydrates (mg)")}</div>}>
+            <Space>
+              <Form.Item noStyle name="minCarb">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+              <Form.Item noStyle name="maxCarb">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+            </Space>
+          </Form.Item>
 
-        <Form.Item label={<div>{t("Fat (mg)")}</div>}>
-          <Space>
-            <Form.Item noStyle name="minFat">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-            <Form.Item noStyle name="maxFat">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-          </Space>
-        </Form.Item>
+          <Form.Item label={<div>{t("Fat (mg)")}</div>}>
+            <Space>
+              <Form.Item noStyle name="minFat">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+              <Form.Item noStyle name="maxFat">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+            </Space>
+          </Form.Item>
 
-        <Form.Item label={<div>{t("Minerals (mg)")}</div>}>
-          <Space>
-            <Form.Item noStyle name="minMinerals">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-            <Form.Item noStyle name="maxMinerals">
-              <InputNumber style={{ width: "100%" }} />
-            </Form.Item>
-          </Space>
-        </Form.Item>
+          <Form.Item label={<div>{t("Minerals (mg)")}</div>}>
+            <Space>
+              <Form.Item noStyle name="minMinerals">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+              <Form.Item noStyle name="maxMinerals">
+                <InputNumber style={{ width: "100%" }} />
+              </Form.Item>
+            </Space>
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 5, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            {<div>{t("Submit")}</div>}
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item wrapperCol={{ offset: 5, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              {<div>{t("Submit")}</div>}
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
     </div>
   );
 };
