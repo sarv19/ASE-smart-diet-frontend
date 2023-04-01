f25b9282992a8f3077e7ce248a8a84e9e8a29b5b
Sarah <vongocquynhnhu@gmail.com>
2023-04-01 13:13:34 -0400

    update diet setting

--- src/components/DietSettings.tsx
+++ src/components/DietSettings.tsx
@@ -42,6 +42,14 @@ const DietSettings = () => {
   };
 
   const onFinish = (values: any) => {
+    localStorage.setItem("minProtein", values.minProtein || '');
+    localStorage.setItem("maxProtein", values.maxProtein || '');
+    localStorage.setItem("minCarb", values.minCarb || '');
+    localStorage.setItem("maxCarb", values.maxCarb || '');
+    localStorage.setItem("minFat", values.minFat || '');
+    localStorage.setItem("maxFat", values.maxFat || '');
+    // localStorage.setItem("minMinerals", values.minMinerals || '');
+    // localStorage.setItem("maxMinerals", values.maxMinerals || '');
     mutate({
       targetCaloriesMax: values.maxCal,
       targetCaloriesMin: values.minCal,
@@ -72,6 +80,14 @@ const DietSettings = () => {
           initialValues={{
             minCal: data.targetCaloriesMin,
             maxCal: data.targetCaloriesMax,
+            minProtein: localStorage?.getItem("minProtein"),
+            maxProtein: localStorage?.getItem("maxProtein"),
+            minCarb: localStorage?.getItem("minCarb"),
+            maxCarb: localStorage?.getItem("maxCarb"),
+            minFat: localStorage?.getItem("minFat"),
+            maxFat: localStorage?.getItem("maxFat"),
+            // minMinerals: localStorage?.getItem("minMinerals"),
+            // maxMinerals: localStorage?.getItem("maxMinerals")
           }}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
@@ -119,7 +135,7 @@ const DietSettings = () => {
             </Space>
           </Form.Item>
 
-          <Form.Item label={<div>{t("Minerals (mg)")}</div>}>
+          {/* <Form.Item label={<div>{t("Minerals (mg)")}</div>}>
             <Space>
               <Form.Item noStyle name="minMinerals">
                 <InputNumber style={{ width: "100%" }} />
@@ -128,7 +144,7 @@ const DietSettings = () => {
                 <InputNumber style={{ width: "100%" }} />
               </Form.Item>
             </Space>
-          </Form.Item>
+          </Form.Item> */}
 
           <Form.Item wrapperCol={{ offset: 5, span: 16 }}>
             <Button type="primary" htmlType="submit">
